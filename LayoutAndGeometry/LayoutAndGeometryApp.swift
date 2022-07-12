069dbb498b4e3f1bb41bf882ab3745aae5000d33
Carson <carsongro@gmail.com>
2022-07-12 13:43:30 -0600

    Initial Commit

---
+++ LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Carson Gross on 7/12/22.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
