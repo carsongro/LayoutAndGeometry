ef38e3629892dc0acd0e7cca5ec2e13ff47dc828
Carson <carsongro@gmail.com>
2022-07-12 20:50:26 -0600

    initial commit

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -8,9 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader { fullView in
+            ScrollView(.vertical) {
+                ForEach(0..<50) { index in
+                    GeometryReader { geo in
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+//                            .background(colors[index % 7])
+                            .background(Color(hue: geo.frame(in: .global).minY / 1000, saturation: 1, brightness: 1))
+                            .rotation3DEffect(.degrees(geo.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                            .opacity(geo.frame(in: .global).minY / 150)
+                            .frame(maxWidth: abs(geo.frame(in: .global).maxY - geo.frame(in: .global).height * 0.5))
+                            .frame(maxWidth: .infinity)
+                            
+                    }
+                    .frame(height: 40)
+                }
+            }
+        }
     }
 }
 
